a53f938ee3235234d2cce71ae0bc3d9a4f12af28
raviasaini <ravia.saini81@outlook.com>
2023-08-21 14:06:44 +0100

    added the items endpoints

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const logger = require('morgan')
 const accounts = require('./accounts')
 const profiles = require('./profiles')
+const items = require('./items')
 
 const app = express()
 
@@ -84,6 +85,47 @@ app.delete('/profile/:id', (req, res) => {
       return res.status(404).json({message: 'Profile not found'})
     }
     profiles.splice(index, 1)
+    res.sendStatus(204)
+})
+
+//ITEMS
+
+app.get('/items', (req, res) => {
+    res.json(items) 
+})
+
+// app.post('/items', (req, res) => {
+//     const item = req.body
+//     console.log("line 24", item)
+//     items.push(item)
+//     res.status(201).json(item)
+// })
+
+app.post('/items', (req, res) => {
+    const newItem = req.body
+    const id = items.length + 1
+    items.push({...newItem, id: parseInt(id)})
+    res.status(201).json(newItem) 
+})
+
+app.get('/items/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    console.log(id)
+    const item = items.find(a => a.id === id)
+    if (!item) {
+      return res.status(404).json({message: 'Account not found'})
+    }
+    res.json(item)
+})
+
+app.delete('/items/:id', (req, res) => {
+    const id = req.params.id
+    const index = items.findIndex(p => p.id === parseInt(id))
+  
+    if (index === -1) {
+      return res.status(404).json({message: 'Profile not found'})
+    }
+    items.splice(index, 1)
     res.sendStatus(204)
 })
 
