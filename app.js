e94a6c58fac512bacb1b2be7362856437aaff1cc
raviasaini <ravia.saini81@outlook.com>
2023-08-21 12:21:57 +0100

    added the account endpoints

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
+const accounts = require('./accounts')
 
 const app = express()
 
@@ -13,6 +14,27 @@ app.get('/', (req, res) => {
       name: "Florin recycle cafe",
       description: "Come and save the environment with us"
     })
+})
+
+app.get('/account', (req, res) => {
+    res.json(accounts)
+})
+  
+app.post('/account', (req, res) => {
+    const account = req.body
+    console.log("line 24", account)
+    accounts.push(account)
+    res.status(201).json(account)
+})
+  
+app.get('/account/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    console.log(id)
+    const account = accounts.find(a => a.id === id)
+    if (!account) {
+      return res.status(404).json({message: 'Account not found'})
+    }
+    res.json(account)
 })
 
 module.exports = app
