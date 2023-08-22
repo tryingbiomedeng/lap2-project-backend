b27789c6bbfea9b7f4bca497a035da4bcfda5db1
SamM71 <sammocock@gmail.com>
2023-08-22 15:50:25 +0100

    setup db in server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
-const db = require('../database/connect')
+const db = require('./db/connect')
 
 const app = express()
 
