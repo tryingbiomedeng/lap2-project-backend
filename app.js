d8684f55ef75e0f42f1719cb96d01748ad0dd848
raviasaini <ravia.saini81@outlook.com>
2023-08-22 11:41:11 +0100

    fixed fixers api

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ app.get('/', (req, res) => {
     })
 })
 
-
 // ACCOUNTS
 
+// Get all accounts
 app.get('/account', async (req, res) => {
   try {
     const { rows } = await db.query('SELECT * FROM accounts')
@@ -28,23 +28,18 @@ app.get('/account', async (req, res) => {
   }
 })
 
-
 // Create new account 
 app.post('/account', async (req, res) => {
   try {
-    const { email, username, password } = req.body
-    
+    const { email, user_name, user_password } = req.body
     const query = `
-      INSERT INTO accounts (email, username, password) 
+      INSERT INTO accounts (email, user_name, user_password) 
       VALUES ($1, $2, $3)
       RETURNING *
     `
-
-    const values = [email, username, password]
-    
+    const values = [email, user_name, user_password]
     const { rows } = await db.query(query, values)
     res.status(201).json(rows[0])
-
   } catch (error) {
     console.error(error)
     res.status(500).json({ message: 'Error creating account' })
@@ -54,63 +49,108 @@ app.post('/account', async (req, res) => {
 // Get single account
 app.get('/account/:id', async (req, res) => {
   try {
-    const { rows } = await db.query('SELECT * FROM accounts WHERE account_id = $1', [req.params.id])
-    
+    const { rows } = await db.query('SELECT * FROM accounts WHERE account_id = $1', [req.params.id]) 
     if (!rows[0]) {
       return res.status(404).json({ message: 'Account not found' })
     }
-
     res.json(rows[0])
-
   } catch (error) {
      console.error(error)
      res.status(500).json({ message: 'Error retrieving account' }) 
   }
 })
 
-//PROFILES
 
-app.get('/profile', (req, res) => {
-    res.json(profiles) 
+//FIXERS
+
+// Get all fixers
+app.get('/fixer', async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM fixers')
+    res.json(rows)  
+  } catch (err) {
+    console.error(err.message)
+  }
 })
 
-app.get('/profile/:id', (req, res) => {
-    const id = req.params.id
-    const profile = profiles.find(p => p.id === parseInt(id))
-    if (!profile) {
-      return res.status(404).json({message: 'Profile not found'})
+// Get single fixer profile
+app.get('/fixer/:id', async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM fixers WHERE fixer_id = $1', [req.params.id]) 
+    if (!rows[0]) {
+      return res.status(404).json({ message: 'Account not found' })
     }
-    res.json(profile)
+    res.json(rows[0])
+  } catch (error) {
+     console.error(error)
+     res.status(500).json({ message: 'Error retrieving account' }) 
+  }
 })
-  
-app.post('/profile', (req, res) => {
-    const newProfile = req.body
-    const id = profiles.length + 1
-    profiles.push({...newProfile, id: parseInt(id)})
-    res.status(201).json(newProfile) 
-})
-  
-app.patch('/profile/:id', (req, res) => {
-    const id = req.params.id
+
+// Create fixer profile
+app.post('/fixer', async (req, res) => {
+  const { user_name, bio, experience } = req.body;
+  try {
+    const accountResult = await db.query('SELECT * FROM accounts WHERE user_name = $1', [user_name]);
+    const account = accountResult.rows[0];
+    if (!account) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    const query = `
+      INSERT INTO fixers (account_id, bio, experience) 
+      VALUES ($1, $2, $3)
+      RETURNING *
+    `;
+    const values = [account.account_id, bio, experience];
+    const { rows } = await db.query(query, values);
+    const insertedFixer = rows[0];
+    res.status(201).json(insertedFixer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error creating fixer' });
+  }
+});
+
+
+// Edit fixer profile
+app.patch('/fixer/:id', async (req, res) => {
+  try {
+    const fixerId = req.params.id
     const updates = req.body
-    const profile = profiles.find(p => p.id === parseInt(id))
-  
-    if (!profile) {
-      return res.status(404).json({message: 'Profile not found'})
+    const query = `
+      UPDATE fixers 
+      SET bio = $1, experience = $2  
+      WHERE fixer_id = $3
+      RETURNING *
+    `
+    const values = [updates.bio, updates.experience, fixerId]
+    const { rows } = await db.query(query, values)
+    if(!rows[0]) {
+      return res.status(404).json({message: 'Fixer not found'})
     }
-    Object.assign(profile, updates)
-    res.json(profile)
+    res.json(rows[0])
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({message: 'Error updating fixer profile'})
+  }
 })
   
-app.delete('/profile/:id', (req, res) => {
-    const id = req.params.id
-    const index = profiles.findIndex(p => p.id === parseInt(id))
-  
-    if (index === -1) {
-      return res.status(404).json({message: 'Profile not found'})
+// Delete fixer profile
+app.delete('/fixer/:id', async (req, res) => {
+
+  try {
+    const fixerId = req.params.id;
+    const query = `DELETE FROM fixers WHERE fixer_id = $1`;
+    await db.query(query, [fixerId]);
+    const result = await db.query('SELECT * FROM fixers WHERE fixer_id = $1', [fixerId]);
+    if(!result.rowCount) {
+      return res.status(404).json({message: 'Fixer not found'});
     }
-    profiles.splice(index, 1)
-    res.sendStatus(204)
+    res.sendStatus(204);
+  } catch (error) {
+     console.error(error);
+     res.status(500).json({message: 'Error deleting fixer profile'}); 
+  }
 })
 
 //ITEMS
