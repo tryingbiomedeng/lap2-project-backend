9fabfd089c2d39f66a38dfe253e4c50f57bf98c5
SamM71 <sammocock@gmail.com>
2023-08-23 18:07:36 +0100

    finish index tests and add showById test

--- __tests__/unit/models/Item.spec.js
+++ __tests__/unit/models/Item.spec.js
@@ -38,5 +38,30 @@ describe('Item', () => {
       const items = await Item.index();
       expect(items).toHaveLength(2);
     })
+
+    it('should throw an error on db query error', async () => {
+      jest.spyOn(db, 'query').mockRejectedValue(new Error())
+
+      try {
+        await Item.index()
+      } catch (error) {
+        expect(error).toBeDefined()
+        expect(error.message).toBe("No items exist")
+      }
+    })
+  })
+
+  describe('showById', () => {
+    it('resolves with an item on successful db query', async () => {
+      let itemData = { item_id: 1, seller_id: 2}
+
+      jest.spyOn(db, 'query')
+        .mockResolvedValueOnce({ rows: [itemData] })
+
+      const result = await Item.showById(1)
+      expect(result).toBeInstanceOf(Item)
+      expect(result.itemId).toBe(1)
+      expect(result.sellerId).toBe(2)
+    })
   })
 })
