98541bff8c3dfddcc22ff82eac95ea73446e68ab
SamM71 <sammocock@gmail.com>
2023-08-23 09:44:08 +0100

    Item.index tests

--- __tests__/unit/models/Item.spec.js
+++ __tests__/unit/models/Item.spec.js
@@ -18,13 +18,25 @@ describe('Item', () => {
     it('should contain the correct values', () => {
       const data = { item_id: 1, seller_id: 2, item_name: "Headphones", item_description: "Cool wired headphones", price: 10.0, available: true }
       const item = new Item(data);
-      console.log(item);
+
       expect(item.itemId).toBe(1);
       expect(item.sellerId).toBe(2);
       expect(item.itemName).toBe('Headphones');
       expect(item.itemDescription).toBe('Cool wired headphones');
       expect(item.price).toBe(10.0);
       expect(item.available).toBe(true);
+    })
+  })
+
+  describe('index', () => {
+    it('resolves with Items when successful', async () => {
+      jest.spyOn(db, 'query')
+        .mockResolvedValueOnce({
+          rows: [{item_name: 'Toaster'}, {item_name: 'Microwave'}]
+        })
+
+      const items = await Item.index();
+      expect(items).toHaveLength(2);
     })
   })
 })
