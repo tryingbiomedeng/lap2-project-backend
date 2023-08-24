ba773099b55fcb6fe4226f1b9c353ce618b99be5
SamM71 <sammocock@gmail.com>
2023-08-24 17:45:59 +0100

    add authenticator to jobs and items

--- routers/items.js
+++ routers/items.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 
 const itemsController = require('../controllers/items');
+const authenticator = require('../middleware/authenticator')
 
 const router = Router();
 
-router.get('/', itemsController.index);
+router.get('/', authenticator, itemsController.index);
 router.get('/:id', itemsController.showById);
 router.post('/', itemsController.create);
 router.patch('/:id', itemsController.update);
